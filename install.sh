0382a04b61b6ffcd10c4c19fa671ec3e3f1f2843
voterol <114055014+voterol@users.noreply.github.com>
2025-02-01 14:18:30 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,23 @@ echo "
 #     # #    # #####  ######    #####    #        ##    ####    #   ###### #    #  ####  ######                                                                         
 "
 
-# Устанавливаем Python 3 и pip
-echo "Устанавливаем Python 3 и pip..."
-sudo apt update
-sudo apt install -y python3 python3-pip
+# Обновляем систему и устанавливаем необходимые пакеты
+echo "Обновляем систему и устанавливаем зависимости..."
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y python3 python3-venv python3-pip
+
+# Создаём виртуальную среду
+echo "Создаём виртуальную среду..."
+python3 -m venv myenv
+
+# Активируем виртуальную среду
+echo "Активируем виртуальную среду..."
+source myenv/bin/activate
 
 # Устанавливаем зависимости из requirements.txt
-echo "Устанавливаем зависимости из requirements.txt..."
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+echo "Устанавливаем зависимости..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
-echo "Скрипт завершен. Установлены зависимости и выведена надпись!"
+echo "Все успешно запущенно!"
